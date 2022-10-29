ca8a0ec34140d3d892e6c853b42c405358924157
ilyashirko <ilyashirko@gmail.com>
2022-10-29 14:58:16 +0300

    add distance with validation

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from django import forms
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
@@ -6,6 +8,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import View
 from foodcartapp.models import Order, Product, Restaurant
+from geopy.distance import distance as calc_distance
 
 
 class Login(forms.Form):
@@ -108,6 +111,47 @@ def get_available_executors(order,
     return available_executors
 
 
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+
+
+def get_restaurants_with_distance(restaurants,
+                                  order,
+                                  ya_geo_api_key=os.getenv('YANDEX_GEO_API_KEY')):
+    order_coordinates = fetch_coordinates(ya_geo_api_key, order.address)
+    available_restaurants = list()
+    for restaurant in get_available_executors(order, restaurants):
+        restaurant_coordinates = fetch_coordinates(ya_geo_api_key, restaurant.address)
+        if order_coordinates is None or restaurant_coordinates is None:
+            distance = None
+        else:
+            distance = round(
+                calc_distance(order_coordinates, restaurant_coordinates).km,
+                1
+            )
+        available_restaurants.append(
+            {
+                'name': restaurant.name,
+                'distance': distance
+            }
+        )
+    return available_restaurants
+
+
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     actual_orders = Order.objects.actual_orders()
@@ -122,11 +166,7 @@ def view_orders(request):
             'address': order.address,
             'price': order.calc_price(),
             'comment': order.comment if order.comment else '',
-            'available_restaurants': [
-                restaurant.name 
-                for restaurant
-                in get_available_executors(order, restaurants)
-            ],
+            'available_restaurants': get_restaurants_with_distance(restaurants, order),
             'executor': order.executor if order.executor else None,
         }
         for order in actual_orders
